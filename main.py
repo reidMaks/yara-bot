ee44a20c9f6767fc7d92a7bffee4727b2e40f88c
kms <reid.maks@gmail.com>
2021-07-28 21:50:19 +0300

    build: Убрал импорт лишних модулей
    При сборке на хероку возникали проблемы

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from bot import bot
 from db import Base, engine
-from alembic import command
-from alembic.config import Config
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    # alembic_cfg = Config("./alembic.ini")
-    # command.stamp(alembic_cfg, "head")
     bot.polling(none_stop=True)
 
