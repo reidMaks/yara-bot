58c1a2a19bce1dcfe126165c63cdcccf69934538
kms <reid.maks@gmail.com>
2021-07-31 23:56:14 +0300

    feat: Добавлена команда автообновления времени последнего приема пищи

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from bot import bot
 from db import Base, engine
+from sheduler import thread
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     bot.polling(none_stop=True)
+
+    thread.start()
 
